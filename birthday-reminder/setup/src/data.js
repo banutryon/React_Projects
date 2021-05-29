2bc0ab3861012347324ce113936b442d0fee8a80
banu tryon <banutryon@gmail.com>
2021-05-28 21:34:35 -0700

    set up birthday reminder

--- birthday-reminder/setup/src/data.js
+++ birthday-reminder/setup/src/data.js
@@ -3,36 +3,31 @@ const people = [
 		id: 1,
 		name: "Sammy Smith",
 		age: 36,
-		image:
-			"https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+		image: "../images/1.jpg",
 	},
 	{
 		id: 2,
 		name: "Sara West",
 		age: 22,
-		image:
-			"https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+		image: "../images/2.jpg",
 	},
 	{
 		id: 3,
 		name: "Mark Matel",
 		age: 56,
-		image:
-			"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+		image: "../images/3.jpg",
 	},
 	{
 		id: 4,
 		name: "Will Wilson",
 		age: 34,
-		image:
-			"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+		image: "../images/4.jpg",
 	},
 	{
 		id: 5,
 		name: "Lola Garten",
 		age: 44,
-		image:
-			"https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+		image: "../images/5.jpg",
 	},
 ];
 
